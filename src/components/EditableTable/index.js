4156fc816c804b4c03de5ed22da84a445cea4ef9
phuocantd <phuocantd@gmail.com>
2019-12-13 22:05:05 +0700

    fix query

--- src/components/EditableTable/index.js
+++ src/components/EditableTable/index.js
@@ -157,7 +157,7 @@ class EditableTable extends React.Component {
     const token = localStorage.getItem('access-token');
     getAllAdministrators(token)
       .then(res => {
-        const arr = res.data.map(obj => ({ ...obj, key: obj._id }));
+        const arr = res.data.data.map(obj => ({ ...obj, key: obj._id }));
         dispatch(setAllAdmin(arr));
         this.setState({
           isLoading: false
--- src/pages/admins/index.js
+++ src/pages/admins/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Divider } from 'antd';
 import EditableTable from '../../components/EditableTable';
-// import EditableTable from '../../components/Table';
 import AddBtn from '../../components/AddBtn';
 
 function AdminPage() {
