19e580c87fb61335bc708e09a001f735cc2f8547
phuocantd <phuocantd@gmail.com>
2019-12-16 03:28:38 +0700

    req single user

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { message } from 'antd';
 
-function UserPage() {
+import { getSingleUser } from '../../api/services/user';
+import {} from '../../actions/users';
+
+function UserPage({ token }) {
   const { id } = useParams();
+
+  useEffect(() => {
+    getSingleUser(id, token)
+      .then(res => console.log(res))
+      .catch(err => {
+        if (err.response) {
+          message.error(err.response.data.error);
+        } else {
+          message.error(err.message);
+        }
+      });
+  });
+
   return <h3>{id}</h3>;
 }
 
-export default UserPage;
+export default connect(state => {
+  return {
+    token: state.tokenReducer
+  };
+})(UserPage);
