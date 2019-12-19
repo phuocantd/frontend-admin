883a3fdc2c6587f8615f34c64b382a4f696ddedf
NGUYỄN PHƯỚC AN <1612009@student.hcmus.edu.vn>
2019-12-20 00:22:38 +0700

    fix name const

--- src/const/admins.js
+++ src/const/admins.js
@@ -4,4 +4,4 @@ export const DEL_ADMIN = 'DELETE_ADMIN';
 
 export const GET_ALL_ADMIN = 'GET_ALL_ADMINs';
 
-export const UPDADTE_ADMIN = 'UPDATE_ADMIN';
+export const UPDATE_ADMIN = 'UPDATE_ADMIN';
